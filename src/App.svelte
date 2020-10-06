d4cbb77b26a056c9d39711b0be51d5731f147a61
vigenere23 <lolgab1@hotmail.com>
2020-10-06 08:56:56 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Component from 'svelte-component'
+
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Component></Component>
+</main>
