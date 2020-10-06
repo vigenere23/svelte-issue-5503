204ce2cd304595e7e2bc332dbdd12eb831319816
vigenere23 <lolgab1@hotmail.com>
2020-10-06 09:14:56 -0400

    added remote component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Component from 'svelte-component'
+	import Component from '@vigenere23/svelte-component'
+	import { default as LocalComponent } from 'svelte-component'
 
 	export let name: string;
 </script>
@@ -8,4 +9,5 @@
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<Component></Component>
+	<LocalComponent></LocalComponent>
 </main>
